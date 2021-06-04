c7d041fb07f8cd835c5fe623a0c9275186279833
Marine Mekhakyan <marinemekhakyan@gmail.com>
2021-06-03 20:47:15 -0700

    assert response is successful, deck ID matches var ID and 50 cards remain in deck

--- test/card-deck.js
+++ test/card-deck.js
@@ -7,7 +7,12 @@ const { assert } = require('chai');
     const deckID = response.data.deck_id;
     //draw two cards from deckID deck
     axios.get(`https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=2`).then(response => {
-        
+        //assert request was successful
+        assert.equal(response.data.success, true, 'Request was not successful');
+        //assert our assigned deck ID matches each new deck ID
+        assert.equal(deckID, response.data.deck_id, 'Response deck id does not match the deck requested');
+        //assert after drawing there are 50 cards left in the deck
+        assert.equal(response.data.remaining, 50, 'Ramainder of cards in the deck after drawing 2 cards does not equal 50');      
         })
 });
 
